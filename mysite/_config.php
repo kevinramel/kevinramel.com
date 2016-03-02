0173a1d81f19d13cba0c5fcc639befb519bb9f01
Kevin Ramel <kevinramel@gmail.com>
2016-03-02 15:35:00 +1300

    2nd commit

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,14 +4,3 @@ global $project;
 $project = 'mysite';
 
 global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'localhost',
-	'path' => ''
-);
-
-// Set the site locale
-i18n::set_locale('en_US');
